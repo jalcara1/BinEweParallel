eeb924862a78c5330cc7f1ca148cfb423aa1ef9b
Julian <jarangol@eafit.edu.co>
2018-06-07 02:42:40 -0500

    lectura del .mew

--- src/controlewe.cpp
+++ src/controlewe.cpp
@@ -9,6 +9,9 @@
 #include <sys/types.h>
 #include <fcntl.h>
 
+#include <map>
+
+
 using namespace std;
 
 char *pMemg;
@@ -44,17 +47,26 @@ int main(int argc, char** argv){
     return 1;
   }
 
-  unsigned int input, Segments = 0, segSize;
-  int memorySegments[10][2];
+  unsigned int input, segSize;
+  unsigned int memg[10][2]; //variable cambiada [segmento][ 0 = inicio][1 = size]
   fstream myReadFileMew(argv[2],ios_base::binary|ios_base::in);
   
+  //manejo de políticas
+  // char:
+  //  0: lectores
+  //  1: escritores
+  //  2: bloqueo
+  //  3: ninguno
+  map<unsigned int,char> mapDataNum;
+  map<unsigned int,char> mapDataStr;
+
   if (myReadFileMew.is_open()) {
     for(int i=0;i<=5;++i){
       myReadFileMew.read((char*)&input,sizeof(int));
-      segSize = input & 0x0000FFFF; // sacamos los 4 bits de la derecha
+      segSize = input & 0xFFFF; // sacamos los 4 bits de la derecha
       input >>= 16; //sacamos los 4 bits de la izquierda (correrlo 16 bits)
       input <<= 2; // multiplicamos por 2^2 
-     
+
       if(i == 2 || i == 4){ //solo si es litstr o .datastr
         if((segSize%4)!= 0 ){
           segSize = segSize-(segSize%4)+4; // no funciona con -=
@@ -63,39 +75,64 @@ int main(int argc, char** argv){
         segSize <<= 2;    //se corre o no? se daña cuando es con string
       }
      
-      memorySegments[i][0] = input; //guardamos el inicio o el fin
-      memorySegments[i][1] = segSize; //guardamos el inicio o el fin
+      memg[i][0] = input; //guardamos el inicio o el fin
+      memg[i][1] = segSize; //guardamos el inicio o el fin
     }
 
-    for(int i=6;i<=10;++i){
+    for(int i=6;i<10;++i){
       myReadFileMew.read((char*)&input,sizeof(int));
-      segSize = input & 0x0000FFFF; // sacamos los 4 bits de la derecha
+      segSize = input & 0xFFFF; // sacamos los 4 bits de la derecha
       input >>= 16; //sacamos los 4 bits de la izquierda (correrlo 16 bits)
 
-      memorySegments[i][0] = input; //guardamos el inicio o el fin
-      memorySegments[i][1] = segSize-input; //guardamos el inicio o el fin
+      memg[i][0] = input; //guardamos el inicio o el fin
+      memg[i][1] = segSize-input; //guardamos el inicio o el fin
     }
 
     for(int i = 0;i<10;++i){
-      cout << "seg:"<< i <<" ini:"<< hex <<memorySegments[i][0]<<" tam:"<<memorySegments[i][1]<<endl;  
+      cout << "seg:"<< i <<" ini:"<< hex <<memg[i][0]<<" tam:"<<memg[i][1]<<endl;  
     }
   
-    size_mem= memorySegments[5][1]+memorySegments[5][0];
+    size_mem= memg[5][0]+memg[5][1];
 
     // creamos la memoria
     createMemory(argv[1]);
     
-    pLitNum = (int *)(pMemg + memorySegments[1][0]);
-    pLitStr = (char *)(pMemg + memorySegments[2][0]); //Char
-    pDataNum = (int *)(pMemg + memorySegments[3][0]);
-    pDataStr = (char *)(pMemg + memorySegments[4][0]); //Char
-    pWorkLoad = (int *)(pMemg + memorySegments[5][0]);
-      
-    while (!myReadFileMew.eof()) {
-      myReadFileMew.read((char*)&input,sizeof(int));
-      
+    pLitNum = (int *)(pMemg + memg[1][0]);
+    pLitStr = (char *)(pMemg + memg[2][0]); //Char
+    pDataNum = (int *)(pMemg + memg[3][0]);
+    pDataStr = (char *)(pMemg + memg[4][0]); //Char
+    pWorkLoad = (int *)(pMemg + memg[5][0]);
+    
+    //almacenar los de las políticas
+    for(int i = 6 ; i < 10;++i){  
+      for(int j = 0; j < memg[i][1] ; ++j){     
+        myReadFileMew.read((char*)&input,sizeof(int));
+        cout<<"in: "<<hex<<input<<endl;
+        segSize = input & 0x0000FFFF; // sacamos los 4 bits de la derecha
+        input >>= 16; //sacamos los 4 bits de la izquierda (correrlo 16 bits)
+        cout << input<< endl;
+        cout << segSize<< endl;
+        if(input >> 15 == 1){
+          //datanum
+          input &= 0x1000;
+          segSize &= 0x1000;
+          for(int k = input; k <= segSize; ++k ){
+            mapDataNum[k]=i%6; //se guarda la posicion sin salto
+            cout <<"datanum =>"<<(int)mapDataNum[k] << endl;
+          }
+        }else{
+          //datastring
+          cout<<"dataString"<<endl;
+          //datastring
+          cout<<"dataString"<<endl;
+          for(int k = input; k <= segSize; ++k ){
+            // cout << "k: "<<k<<endl;
+            mapDataStr[k]=i%6; //se guarda la posicion en datanum
+          }
+        }
 
-    }  
+      }
+    }
   }
   myReadFileMew.close();
   // for (;;) {
@@ -110,11 +147,6 @@ int main(int argc, char** argv){
   //   *(pWorkLoad) = 32;    
   //   sleep(3);
   // }
-  
-
-
-
-  
   
   // int bewFile =3; //Argument 3 -> After Memory Name
   // int argcs = argc;
